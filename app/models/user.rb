1e05fd7d565bbae3a7ebf492bc9b6f87071a4657
yossu-y <yangyuub@gmail.com>
2022-05-04 14:17:27 +0000

    [add]ActiveStorage

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,12 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { minimum: 2, maxmum: 20 }
   validates :introduction, length: { maximum: 50 }
 
+  def get_profile_image(width,height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [100, 100]).processed
+  end
+
 end
