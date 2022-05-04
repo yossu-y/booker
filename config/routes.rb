c88bf6122e6db6c0514794db5ce551938af0bc97
yossu-y <yangyuub@gmail.com>
2022-05-04 13:05:46 +0000

    [add]controller-users,books

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   root to: "homes#top"
   get "homes/about" => "homes#about"
