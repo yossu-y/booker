df73a3fa9e8863351268165770735a0068ad645e
yossu-y <yangyuub@gmail.com>
2022-05-05 00:14:43 +0000

    [add]views-books-edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,5 +12,12 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
   end
+
 end
