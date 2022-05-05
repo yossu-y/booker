0d41cec730e1e66815c21fae17e2f64327d66ac8
yossu-y <yangyuub@gmail.com>
2022-05-05 14:14:42 +0000

    [update]books-controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+   before_action :correct_user, only: [:edit, :update]
+
   def index
     @book = Book.new
     @books = Book.all
@@ -18,6 +20,24 @@ class BooksController < ApplicationController
     else
       redirect_to books_path
     end
+  end
+
+  def destroy
+    @book = Book,find(params[:id])
+    if @book.destroy
+      redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
   end
 
 end
